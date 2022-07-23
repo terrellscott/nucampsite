f08c2da9938e8bf2590a378d60f961ef4376f389
Terrell Scott <terrelldupree@gmail.com>
2022-07-23 09:49:24 -0400

    Homepage Display

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,8 +4,11 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find(campsite => campsite.id === id);
 };
 
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
 
